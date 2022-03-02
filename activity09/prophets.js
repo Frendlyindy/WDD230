9ff13de5ab7371fc4f266040df53ad46f605541d
Frendlyindy <frendlyindian@gmail.com>
2022-03-02 09:17:05 -0700

    commit

--- activity09/prophets.js
+++ activity09/prophets.js
@@ -12,15 +12,21 @@ function displayProphets(prophet) {
     let p2 = document.createElement('p');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-  
+    //h2.textContent = prophet.name + ' ' + prophet.lastname;
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    p1.textContent = `Birthplace: ${prophet.birthplace}`;
+    p2.textContent = `Birthdate: ${prophet.birthdate}`;
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
+    let image = portrait;
     image.setAttribute('src', prophet.imageurl);
-    image.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    //image.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    image.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     image.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(p2);
+    card.appendChild(p1);
     card.appendChild(image);
   
     // Add/append the existing HTML div with the cards class with the section(card)
@@ -37,4 +43,4 @@ fetch(url)
     prophets.forEach(displayProphets);
 
     
-})
+});
