ca77fd4be9115866265317ac7cf4a2438438a228
Frendlyindy <frendlyindian@gmail.com>
2022-03-09 08:59:59 -0700

    commit

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -11,11 +11,11 @@ const url = "https://api.openweathermap.org/data/2.5/weather?id=4887398&appid=7b
 
             //let city = data.name;
             //document.querySelector('#city').innerText = city;
-            document.querySelector('#cond').innerText = data.weather[0].description;
+            document.querySelector('#cond').innerText = data.weather[0].description.toUpperCase();
 
             //https://openweathermap.org/img/wn/10d@2x.png
             let iconId = data.weather[0].icon;
-            let iconSource = `http://openweathermap.org/img/wn/${iconId}@4x.png`;
+            let iconSource = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
             document.querySelector('#weather-icon').setAttribute('src', iconSource);
 
         })
