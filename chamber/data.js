46c43ac870716e2e4030a8c431c0c6c4a529966d
Frendlyindy <frendlyindian@gmail.com>
2022-03-29 23:14:45 -0600

    commit

--- chamber/data.js
+++ chamber/data.js
@@ -1,11 +1,14 @@
-const url = "data.json";
+const url = "./data.json";
+const resultElement = document.querySelector('div#directory');
 fetch(url)
 .then(response => response.json())
 .then(data => {
+    
     for(let power of businesses)
             {
                 const s = document.createElement('section');
                 s.textContent = power;
+                resultElement.appendChild(s);
                 
             }
 })
